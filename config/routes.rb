d5c4f97cb14bc65fb7244b30ab6519e1c5e93d4f
Yasu <yasuharu_matsuno@berkeley.edu>
2016-09-29 23:31:57 +0000

    added home help tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
-  get 'static_pages/help'
-
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
+  match 'help', to: 'static_pages#help', via: 'get'
+  
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
