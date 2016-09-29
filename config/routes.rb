3f49b08fc1cae070c7bae46376eb6848d739b994
Yasu <yasuharu_matsuno@berkeley.edu>
2016-09-29 23:42:36 +0000

    Changed home page path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   match 'help', to: 'static_pages#help', via: 'get'
+  match 'home', to: 'static_pages#home', via: 'get'
   
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
